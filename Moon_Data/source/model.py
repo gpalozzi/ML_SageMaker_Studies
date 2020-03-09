69f5271ca538acf7274495db86a4ac09cf492dc9
Gabriele Palozzi <gpalozzi@gmail.com>
2020-03-09 09:03:52 +0000

    Exercises

--- Moon_Data/source/model.py
+++ Moon_Data/source/model.py
@@ -14,6 +14,16 @@ class SimpleNet(nn.Module):
          '''
         super(SimpleNet, self).__init__()
         
+        self.input_dim = input_dim
+        # linear layer (784 -> hidden_1)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        # linear layer (n_hidden -> hidden_2)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        # dropout layer (p=0.2)
+        # dropout prevents overfitting of data
+        self.dropout = nn.Dropout(0.2)
+
+        
         # define all layers, here
         
     
@@ -23,6 +33,11 @@ class SimpleNet(nn.Module):
            :param x: A batch of input features
            :return: A single, sigmoid activated value
          '''
-        # your code, here
-        
-        return x
+        x = x.view(-1, self.input_dim)
+        # add hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add hidden layer, with relu activation function
+        x = F.sigmoid(self.fc2(x))
+        return x        
